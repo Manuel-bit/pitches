1614dfbbf2d6fcfeb22d6f0da475dabc4bd615ce
manuel.bit <emmanuel_njuguna@outlook.com>
2019-11-22 14:45:15 +0300

    add the signup template and form validation

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField,PasswordField,SubmitField,validators
 from wtforms.validators import EqualTo,DataRequired
+from wtforms import ValidationError
 
 class SignupForm(FlaskForm):
   username = StringField('User Name',[validators.DataRequired()])
@@ -8,3 +9,11 @@ class SignupForm(FlaskForm):
   password = PasswordField('Password',[validators.DataRequired(), validators.EqualTo("confirm", message='passwords must match')])
   confirm = PasswordField('Confir Password',[validators.DataRequired()])
   submit = SubmitField('Sign Up')
+
+  def check_email(self,dataf_ield):
+    if User.query.filter_by(email = data_field.data).first():
+      raise ValidationError('an account with this email exist')
+
+  def check_username(self,data_field):
+    if User.query.filter_by(username = data_field).first():
+      raise ValidationError('username taken, choose another username..')
--- app/auth/views.py
+++ app/auth/views.py
@@ -1,15 +1,16 @@
 from . import auth
-from flask import render_template,redirect
+from flask import render_template,redirect,url_for
 from .forms import SignupForm
 from ..models import User
+from .. import db
 
 @auth.route('/signup',methods = ["GET","POST"])
 def signup():
+  title='Sign up'
   form = SignupForm()
   if form.validate_on_submit():
     user = User(username=form.username.data,email=form.email.data,pass_secure=form.password.data)
     db.session.add(user)
     db.session.commit()
     return redirect(url_for('main.index'))
-  title='Sign up'
   return render_template('signup.html',form=form,title=title)
