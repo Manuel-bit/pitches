08569860f5b64e40714b3cae8aa71c1b94e694de
manuel.bit <emmanuel_njuguna@outlook.com>
2019-11-28 18:08:41 +0300

    deploying to heroku

--- manage.py
+++ manage.py
@@ -1,8 +1,7 @@
-
+from flask_script import Manager,Server
 from app import create_app,db
 from app.models import User,Pitch,Comment
-from  flask_migrate import Migrate, MigrateCommand
-from flask_script import Manager,Server
+from flask_migrate import Migrate,MigrateCommand
 
 
 app = create_app('production')
