457a51131f5d82d56bc800671d31f9eb9f28caf2
manuel.bit <emmanuel_njuguna@outlook.com>
2019-11-28 17:40:36 +0300

    deploying to heroku

--- app/__init__.py
+++ app/__init__.py
@@ -6,15 +6,15 @@ from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
 
 
-
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
 
 db=SQLAlchemy()
 bootstrap = Bootstrap()
 
 
-login_manager = LoginManager()
-login_manager.session_protection = 'strong'
-login_manager.login_view = 'auth.login'
+
 
 
 def create_app(config_name):
